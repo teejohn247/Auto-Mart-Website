b95c4524239fceef682d644c59ba2861781511e8
teejohn247 <teejohn247@gmail.com>
2019-05-17 01:04:16 +0100

    feature(admin-view-delete):add a page where the admin can view all posted ads and delete any [finishes #166075257]

---
+++ UI/js/menu.js
@@ -0,0 +1,15 @@
+      // Get the modal
+      var modal = document.getElementById("myModal");
+      var btn = document.getElementById("myB");      
+      var span = document.getElementsByClassName("close")[0];      
+      btn.onclick = function() {
+        modal.style.display = "block";
+      }      
+      span.onclick = function() {
+        modal.style.display = "none";
+      }      
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
