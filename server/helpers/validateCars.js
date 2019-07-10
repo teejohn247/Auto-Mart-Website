4f3f5898c245a6b256ae72e6d3acb9828a490b73
teejohn247 <teejohn247@gmail.com>
2019-07-11 00:34:42 +0100

    chore(refactor): refactor codes to reduce smells and duplications [finishes #166860337]

--- server/helpers/validateCars.js
+++ server/helpers/validateCars.js
@@ -4,6 +4,7 @@ const validateCars = {
 
   validation(newAd) {
     const newAdSchema = {
+      owner: Joi.number().required(),
       product_image: Joi.string().required(),
       state: Joi.string().valid('new', 'used').required(),
       status: Joi.string().required(),
