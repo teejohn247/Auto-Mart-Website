ffaaa809999683b2120d7aa0716bc4b4df548512
teejohn247 <teejohn247@gmail.com>
2019-07-13 02:15:58 +0100

    chore(refactor): refactor codes to reduce smells and duplications [finishes #166860337]

--- server/controllers/viewAll.js
+++ server/controllers/viewAll.js
@@ -1,8 +1,41 @@
+/* eslint-disable camelcase */
 import pool from '../models/database';
 
 const allposted = async (req, res) => {
+  let findCars = 'SELECT * FROM cars';
+  if (req.originalUrl.includes('?'));
+
+  if (req.query.status !== undefined) {
+    findCars += ` WHERE status = '${status}'`;
+  }
+
+  const { state } = req.query;
+  if (state !== undefined) {
+    findCars += ` WHERE state = '${state}'`;
+  }
+
+  const { manufacturer } = req.query;
+if (manufacturer !== undefined) {
+findCars += ` WHERE manufacturer = '${manufacturer}'`;
+}
+const { body_type } = req.query;
+if (body_type !== undefined) {
+  findCars += ` AND body_type = '${body_type}'`;
+}
+
+const { owner } = req.query;
+if (owner !== undefined) {
+  findCars += ` AND owner_id = '${owner}'`;
+}
+
+  let { min_price, max_price } = req.query;
+if (min_price !== undefined || max_price !== undefined) {
+if (min_price === undefined) min_price = 1000;
+if (max_price === undefined) max_price = Number.MAX_VALUE;
+findCars += ` WHERE price >= ${min_price} AND price <= ${max_price}`;
+}
+
   try {
-    const findCars = 'SELECT * FROM cars';
     const cars = await pool.query(findCars);
     if (!cars.rows[0]) {
       res.status(404).json({
