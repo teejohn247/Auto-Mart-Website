1bca1cf0c7afa5b1e2bc56977b1cfd895dced709
teejohn247 <teejohn247@gmail.com>
2019-07-16 20:22:14 +0100

    chore(refactor): refactor codes to reduce smells and duplications [finishes #166860337]

---
+++ server/routes/adminAll.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import auth from '../middleware/auth';
+import allposted from '../controllers/viewAll';
+import admin from '../middleware/adminAuth';
+
+
+const router = express.Router();
+
+router.get('/allposted', [auth, admin], allposted);
+
+
+export default router;
