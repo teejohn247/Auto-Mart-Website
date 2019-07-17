871f87030c5e966a2b2d8d960725d9734ffaf029
teejohn247 <teejohn247@gmail.com>
2019-07-17 05:19:07 +0100

    feature(code-clean-up):refactor the codes [finishes #166078814]

---
+++ UI/js/fetchApi/purchase.js
@@ -0,0 +1,37 @@
+const is_loggedin = localStorage.getItem('is_loggedin');
+
+window.onload = () => {
+  if (!is_loggedin) {
+    window.location.href = '../UI/index.html';
+	}
+const user_id = localStorage.getItem('user_id');
+const is_loggedin = localStorage.getItem('is_loggedin');
+const token = JSON.parse(localStorage.getItem('authToken'));
+const storage = JSON.parse(localStorage.getItem('storageName'));
+
+const order = document.querySelector("#order1");
+
+order.onclick = (e) => {
+	e.preventDefault();
+	const amount = document.querySelector(".amount");
+    fetch(
+    "http://automart-webapp.herokuapp.com/api/v1/order", {
+        method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": token
+		},
+		body: JSON.stringify({  car_id: storage.data.id,  amount: amount.value })
+	})
+		.then(res => res.json())
+		.then(data => {
+		localStorage.setItem("order", JSON.stringify(data));
+      if (data.status !== 201) {
+			} else {
+		console.log(data);			
+
+			}
+			})
+		.catch(error => console.log(error.message));
+}
+}
